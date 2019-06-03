70781b798d945b5ab29d55346ad215621749d877
Aleksei <ziramax009@gmail.com>
2019-06-03 23:21:53 +0600

    my_branch

--- app/config/router.php
+++ app/config/router.php
@@ -2,6 +2,19 @@
 
 $router = $di->getRouter();
 
+
+$router->add('/:controller/:action/:params', [
+    'namespace' => 'App\Controllers',
+    'controller' => 1,
+    'action' => 2,
+    'params' => 3,
+]);
+
+$router->add('/:controller', [
+    'namespace' => 'App\Controllers',
+    'controller' => 1
+]);
+
 // Define your routes here
 
 $router->handle();
