5d286fe1664d2caafb105321dbc2abff8a467def
Aleksei009 <ziramax009@gmail.com>
2019-06-04 08:33:06 +0600

    my-first-commit

--- app/config/router.php
+++ app/config/router.php
@@ -2,6 +2,15 @@
 
 $router = $di->getRouter();
 
+$router->add('/confirm/{code}/{email}', [
+    'controller' => 'user_control',
+    'action' => 'confirmEmail'
+]);
+$router->add('/reset-password/{code}/{email}', [
+    'controller' => 'user_control',
+    'action' => 'resetPassword'
+]);
+
 // Define your routes here
 
 $router->handle();
